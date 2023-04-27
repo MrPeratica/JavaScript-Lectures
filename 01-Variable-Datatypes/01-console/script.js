c953f2202b86b28d8243407382539a133c4f26d2
unknown <cardinesarchie@gmail.com>
2023-04-27 15:08:33 +0800

    Initial Commit

---
+++ 01-Variable-Datatypes/01-console/script.js
@@ -0,0 +1,28 @@
+// alert('Hello from JS file');
+
+// log a number
+console.log(100)
+
+// log string 
+console.log("Hello World");
+
+// log multiple values 
+console.log(20, "Hello", true);
+
+// log variable
+const x = 100;
+console.log(x);
+
+// Console error & warning
+console.error("Alert");
+console.warn("Warning");
+
+//log object as a table
+console.log({ name: "Ryan", email: "ryan@gmail.com" });
+
+//Group Console Commands
+console.group("Simple");
+console.log(x);
+console.error("Alert")
+console.warn("Warning");
+console.groupEnd();
