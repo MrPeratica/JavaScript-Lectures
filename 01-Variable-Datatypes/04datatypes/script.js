80ba978af0791226f1ad7bae02342efa2b99941d
unknown <cardinesarchie@gmail.com>
2023-04-27 17:03:28 +0800

    reamrks

---
+++ 01-Variable-Datatypes/04datatypes/script.js
@@ -0,0 +1,22 @@
+//Ways to declare a variable 
+// war, let & const
+
+let firstName = "Juan";
+const lastName = "Cruz";
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+//Naming Conventions 
+// Only Letters, numbers, underscores and dollar sign
+// Can't start with a number 
+
+// Multi - Word Formatting
+// firstName = camelCase
+// first_name 
+
+console.log(x);
+
+const arr = [1,2,3,4]
